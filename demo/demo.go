bea8e4de7c42be007c7ed7a2f7dbd28eb5ae450f
Marcelle von Wendland <mvw@bancstreet.com>
2015-08-07 10:00:47 +0100

    init

--- demo/demo.go
+++ demo/demo.go
@@ -3,24 +3,76 @@ package main
 import (
         "github.com/vonwenm/goserial"
         "log"
+        "time"
 )
 
 func main() {
-        c := &serial.Config{Name: "COM1", Baud: 9600 }
-        s, err := serial.OpenPort(c)
-        if err != nil {
-                log.Fatal(err)
-        }
 
-        n, err := s.Write([]byte("test"))
-        if err != nil {
-                log.Fatal(err)
+        log.Print("CONNECTING")
+    
+        c := &serial.Config{Name: "COM4", Baud: 9600 }
+            s, err := serial.OpenPort(c)
+            if err != nil {
+                    log.Fatal(err)
         }
+      
+        log.Print("CONNECTED")
+             
+        test := 10
+        
+        for (test == 10) {
+        
+        //======================
+        
 
-        buf := make([]byte, 128)
-        n, err = s.Read(buf)
-        if err != nil {
-                log.Fatal(err)
+                var msg01 string  = "m1 on"
+                
+		n, err := s.Write([]byte( msg01 ))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+                log.Print( "SND:  ", msg01 )
+
+		buf := make([]byte, 128)
+		n, err = s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	        sx0 := string( buf[:n] )
+	        
+	        log.Print("REC: ", sx0 )
+        
+       //==================
+        
+        	time.Sleep(time.Second * 6 )
+        
+       //==================
+          
+                var msg02 string  = "m1 off"
+	        
+	        n1, err1 := s.Write([]byte( msg02  ))
+	        if err1 != nil {
+	                log.Fatal(err1)
+	        }
+	
+                log.Print( "SND:  ", msg02 )
+	        
+	        buf1 := make([]byte, 128)
+	        n1, err1 = s.Read(buf1)
+	        if err1 != nil {
+	                log.Fatal(err1)
+	        }
+	        
+	        sx1 := string( buf1[:n1] )
+	        
+	        log.Print("REC: ", sx1 )
+	        
+       //==================
+          
+       		time.Sleep(time.Second * 6  )
+        
+        
         }
-        log.Print("%q", buf[:n])
 }
